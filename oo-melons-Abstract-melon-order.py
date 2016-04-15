a475cf778c99b2400dded813bfad2493842d2210
Hackbright Student <info@hackbrightacademy.com>
2016-04-14 18:12:21 -0700

    Refactoring Domestic and International subclasses

--- oo-melons-Abstract-melon-order.py
+++ oo-melons-Abstract-melon-order.py
@@ -11,15 +11,13 @@ class AbstractMelonOrder(object):
     #Therefore, an initalization has been done under the superclass with the following attributes : species, qty, shipped, country_code. 
         #country_code is only relevant to the international subclass, therefore the country code constructor has been set to "None".
 
-
-    def __init__(self, species, qty, country_code=None):
+    def __init__(self, species, qty, order_type=None, tax=None):
         """Initialize melon order attributes"""
-
-
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.country_code = country_code
+        self.tax = tax
+        # self.country_code = country_code
 
     def get_total(self):
         """Calculate price."""
@@ -38,19 +36,44 @@ class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
 
 
-    order_type = "domestic"
-    tax = 0.08
+    def __init__(self, species, qty, order_type=None, tax=None):
+        super(DomesticMelonOrder, self).__init__(species, qty, 'domestic', 0.08)
+    # order_type = "domestic"
+    # tax = 0.08
 
 
-class InternationalMelonOrder(AbstractMelonOrder):
-    """An international (non-US) melon order."""
+# class InternationalMelonOrder(AbstractMelonOrder):
+#     """An international (non-US) melon order."""
 
-    # country_code = country_code
-    order_type = "international"
-    tax = 0.17
+#     # country_code = country_code
+#     order_type = "international"
+#     tax = 0.17
 
 
-    def get_country_code(self):
-        """Return the country code."""
+#     def get_country_code(self):
+#         """Return the country code."""
 
-        return self.country_code
+# return self.country_code
+
+
+
+
+"""def mult (num1, num2):
+    return num1 * num2
+
+product = mult(8, 7)
+my_num = num1
+
+
+class AbstractOrder(object):
+
+    def __init__(self, qty, melon_type, order_type, country_code=None):
+        self.name = name
+
+  
+
+class InternationalMelonOrder(AbstractOrder):
+    
+    def __init__(self, qty, melon_type, country_code):
+        super(InternationalMelonOrder, self).__init__(qty, melon_type, 'international', country_code)
+"""
